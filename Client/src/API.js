388c9801ef50c48e7750392ce4644f99f8d85166
vPrateek10 <vermaprateek035@gmail.com>
2022-08-20 13:49:01 +0530

    First commit

---
+++ Client/src/API.js
@@ -0,0 +1,7 @@
+export const LOGIN_ENDPOINT = process.env.REACT_APP_BACKEND_URL + "/api/auth/login";
+export const SIGNUP_ENDPOINT = process.env.REACT_APP_BACKEND_URL + "/api/auth/register";
+export const GET_USER_ENDPOINT = process.env.REACT_APP_BACKEND_URL + "/api/auth/user";
+export const WISHLIST_ENDPOINT = process.env.REACT_APP_BACKEND_URL + "/api/wishlist/";
+export const PREDICTION_ENDPOINT = process.env.REACT_APP_BACKEND_URL + "/api/predprices";
+export const PRICE_HISTORY_ENDPOINT = process.env.REACT_APP_BACKEND_URL + "/api/trackedprices";
+export const DELETE_WISHLIST_ENDPOINT = process.env.REACT_APP_BACKEND_URL + "/api/deleteWishlist";
